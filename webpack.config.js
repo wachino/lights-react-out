5375f546a3269e1b9500d327f3bba908f1455a65
wachino <juanandc@ucm.es>
2018-01-27 00:35:18 +0100

    [build] Added build configuration files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'lights.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
