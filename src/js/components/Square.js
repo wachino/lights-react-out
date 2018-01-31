24b7bb0e644369617d60ebebccf49d3ce88fcd52
wachino <juanandc@ucm.es>
2018-01-31 20:01:37 +0100

    [LRO][style] Added eslint rules

--- src/js/components/Square.js
+++ src/js/components/Square.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import css from 'css/Square.css';
+import 'css/Square.css';
 
 export default class Square extends React.Component {
   render () {
     return (
-      <button className={'square' + (this.props.isOn? ' isOn' : '')} onClick={this.props.onClick}>
+      <button className={'square' + (this.props.isOn ? ' isOn' : '')} onClick={this.props.onClick}>
         {this.props.value}
       </button>
     );
