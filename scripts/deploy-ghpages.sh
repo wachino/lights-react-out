bc0e28692cf45742d9b802a224df85dcfca23b2d
AnguloHerrera <nanguloherrera@gmail.com>
2018-02-10 20:28:14 +0100

    [LRO-6][feat] Fixed step doc-page into circleci config file.

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -20,30 +20,30 @@ else
 fi
 if [ ! -z $3 ]
 then
-    gh-branch=$3
+    gh_branch=$3
 else
-    gh-branch=gh-pages
+    gh_branch="gh-pages"
 fi
 
 # make a directory to put the gp-pages branch
-mkdir ${gh-branch}-branch
-cd ${gh-branch}-branch
-# now lets setup a new repo so we can update the ${gh-branch} branch
+mkdir $gh_branch-branch
+cd $gh_branch-branch
+# now lets setup a new repo so we can update the $gh_branch branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
 
-# switch into the the ${gh-branch} branch
-if git rev-parse --verify origin/${gh-branch} > /dev/null 2>&1
+# switch into the the $gh_branch branch
+if git rev-parse --verify origin/$gh_branch > /dev/null 2>&1
 then
-    git checkout ${gh-branch}
+    git checkout $gh_branch
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
 else
-    git checkout --orphan ${gh-branch}
+    git checkout --orphan $gh_branch
 fi
 
 # copy over or recompile the new site
@@ -51,13 +51,13 @@ cp -a "../${distFolder}/." .
 
 # stage any changes and new files
 git add -A
-# now commit, ignoring branch ${gh-branch} doesn't seem to work, so trying skip
+# now commit, ignoring branch $gh_branch doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin ${gh-branch}
-# go back to where we started and remove the ${gh-branch} git repo we made and used
+git push --force --quiet origin $gh_branch
+# go back to where we started and remove the $gh_branch git repo we made and used
 # for deployment
 cd ..
-rm -rf ${gh-branch}-branch
+rm -rf $gh_branch-branch
 
 echo "Finished Deployment into Github Pages."
