ce7db1089c244f8ffaa2ca3d6d47ed01e9349666
AnguloHerrera <nanguloherrera93@gmail.com>
2018-02-10 20:10:05 +0100

    [LRO-6][feat] Added jsdoc generation in CI process.

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -5,28 +5,45 @@ set -e
 
 # show where we are on the machine
 pwd
-remote=$(git config remote.origin.url)
+
+# Arguments
+# $1: distFolder
+# $2: remote github URL. Default: remote origin
+# $3: repo branch. Default: gh-pages
+
 distFolder=$1
+if [ ! -z $2 ]
+then
+    remote=$2
+else
+    remote=$(git config remote.origin.url)
+fi
+if [ ! -z $3 ]
+then
+    gh-branch=$3
+else
+    gh-branch=gh-pages
+fi
 
 # make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
+mkdir ${gh-branch}-branch
+cd ${gh-branch}-branch
+# now lets setup a new repo so we can update the ${gh-branch} branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
 
-# switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+# switch into the the ${gh-branch} branch
+if git rev-parse --verify origin/${gh-branch} > /dev/null 2>&1
 then
-    git checkout gh-pages
+    git checkout ${gh-branch}
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
 else
-    git checkout --orphan gh-pages
+    git checkout --orphan ${gh-branch}
 fi
 
 # copy over or recompile the new site
@@ -34,13 +51,13 @@ cp -a "../${distFolder}/." .
 
 # stage any changes and new files
 git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+# now commit, ignoring branch ${gh-branch} doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
-# go back to where we started and remove the gh-pages git repo we made and used
+git push --force --quiet origin ${gh-branch}
+# go back to where we started and remove the ${gh-branch} git repo we made and used
 # for deployment
 cd ..
-rm -rf gh-pages-branch
+rm -rf ${gh-branch}-branch
 
 echo "Finished Deployment into Github Pages."
--- src/js/components/Square.js
+++ src/js/components/Square.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'css/Square.css';
 
+/**
+ * @constructor
+ * @name Square
+ * @description Square react component
+ */
 export default class Square extends React.Component {
+  /**
+   * @memberof! Square#
+   * @description Render a Square
+   */
   render () {
     return (
       <div className='cell'>
@@ -17,11 +26,23 @@ export default class Square extends React.Component {
     );
   }
 
+  /**
+   * @memberof! Square#
+   * @description TODO
+   * @param {object} nextProps TODO
+   * @param {object} [nextState] TODO (Example optional)
+   * @todo Example (only for testing)
+   */
   shouldComponentUpdate (nextProps, nextState) {
     return this.props.isOn !== nextProps.isOn;
   }
 }
 
+/**
+ * @type {object}
+ * @property {PropTypes.bool} isOn TODO
+ * @property {PropTypes.func} onClick TODO
+ */
 Square.propTypes = {
   isOn: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired
